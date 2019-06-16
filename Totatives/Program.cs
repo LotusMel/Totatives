aaa187d3497daf3176f094f98766fe56a996d006
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-06-15 23:00:26 -0500

    Add project files.

---
+++ Totatives/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Totatives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 10; n++)
+            
+            Console.WriteLine("phi(" + n + ") = " + Phi(n));
+            Console.ReadLine();
+        }
+
+        static int GetGCD (int a, int b)
+        {
+            if (a == 0)
+                return b;
+            return GetGCD(b % a, a);   
+        }
+
+        static int Phi(int n)
+        {
+            int result = 1;
+            for (int i = 2; i < n; i++)
+                if (GetGCD(i, n) == 1)
+                    result++;
+            return result;
+        }
+        
+    }
+}
