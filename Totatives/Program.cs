074da2612aa4e410fca33a337ef091d7f5350521
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-06-18 23:07:42 -0500

    Added accessability modifiers

--- Totatives/Program.cs
+++ Totatives/Program.cs
@@ -4,8 +4,7 @@ namespace Totatives
 {
     class Program
     {
-
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             for (int n = 1; n <= 10; n++)
             
@@ -13,14 +12,14 @@ namespace Totatives
             Console.ReadLine();
         }
 
-        static int GetGCD (int a, int b)
+        private static int GetGCD (int a, int b)
         {
             if (a == 0)
                 return b;
             return GetGCD(b % a, a);   
         }
 
-        static int Phi(int n)
+        private static int Phi(int n)
         {
             int result = 1;
             for (int i = 2; i < n; i++)
